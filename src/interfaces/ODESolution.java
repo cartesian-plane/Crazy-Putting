ae05fe5f6001b1bb62d8d06ff48fee9d70fefca6
Andrei <87477311+cartesian-plane@users.noreply.github.com>
2024-03-19 22:50:13 +0100

    Add timeTaken field to ODESolution object

--- src/interfaces/ODESolution.java
+++ src/interfaces/ODESolution.java
@@ -5,9 +5,21 @@ import java.util.ArrayList;
 public class ODESolution {
     ArrayList<Number> time;
     ArrayList<ArrayList<Number>> stateVectors;
+    /**
+     * The time taken (in ms) to compute the solution.
+     */
+    private long timeTaken;
 
     public ODESolution(ArrayList<Number> time, ArrayList<ArrayList<Number>> stateVectors) {
         this.time = time;
         this.stateVectors = stateVectors;
+    }
+
+    public long getTimeTaken() {
+        return timeTaken;
+    }
+
+    public void setTimeTaken(long timeTaken) {
+        this.timeTaken = timeTaken;
     }
 }
