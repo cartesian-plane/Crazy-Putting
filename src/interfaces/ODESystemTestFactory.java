db813e1694b641b47b0ddf3afc09189191e9c54b
Andrei <87477311+cartesian-plane@users.noreply.github.com>
2024-03-20 14:38:20 +0100

    Add overloaded methods in ODESystemTestFactory

--- src/interfaces/ODESystemTestFactory.java
+++ src/interfaces/ODESystemTestFactory.java
@@ -17,6 +17,38 @@ public class ODESystemTestFactory {
 
     private ODESystem LotkaVolterra(double initialX, double initialY, double alpha, double beta,
                                     double delta, double gamma) {
+
+        if (alpha <= 0 || beta <= 0 || gamma <= 0 || delta <= 0) {
+            throw new IllegalArgumentException("alpha, beta, gamma, and delta must be positive");
+        }
+
+        ArrayList<Number> initialStateVector = new ArrayList<>();
+        ArrayList<IFunc<Number, Number>> functions = new ArrayList<>();
+
+        // initial conditions
+        ArrayList<Number> initialValues = new ArrayList<>();
+        initialValues.add(initialX);
+        initialValues.add(initialY);
+
+        initialStateVector.add(initialValues.get(0));
+        initialStateVector.add(initialValues.get(1));
+
+
+        functions.add( (systemVars) -> {return alpha*systemVars.get(0).doubleValue() - beta*systemVars.get(0).doubleValue()
+                *systemVars.get(1).doubleValue(); });
+
+        functions.add( (systemVars) -> {return delta*systemVars.get(0).doubleValue()*systemVars.get(1).doubleValue() -
+                gamma*systemVars.get(1).doubleValue(); });
+
+        return new ODESystem(initialStateVector, functions);
+    }
+
+    private ODESystem LotkaVolterra(double initialX, double initialY) {
+
+        double alpha = 1;
+        double beta = 1;
+        double delta = 1;
+        double gamma = 1;
         ArrayList<Number> initialStateVector = new ArrayList<>();
         ArrayList<IFunc<Number, Number>> functions = new ArrayList<>();
 
@@ -51,6 +83,38 @@ public class ODESystemTestFactory {
     }
 
     private ODESystem SIR(double initialS, double initialI, double initialR, double k, double mu, double gamma) {
+        ArrayList<Number> initialStateVector = new ArrayList<>();
+        ArrayList<IFunc<Number, Number>> functions = new ArrayList<>();
+
+        // initial conditions
+        ArrayList<Number> initialValues = new ArrayList<>();
+        initialValues.add(initialS);
+        initialValues.add(initialI);
+        initialValues.add(initialR);
+
+        initialStateVector.add(initialValues.get(0));
+        initialStateVector.add(initialValues.get(1));
+        initialStateVector.add(initialValues.get(2));
+
+
+        functions.add( (systemVars) -> {return -k*systemVars.get(0).doubleValue()*systemVars.get(1).doubleValue() +
+                mu*(1 - systemVars.get(0).doubleValue()); });
+
+        functions.add( (systemVars) -> {return k*systemVars.get(0).doubleValue()*systemVars.get(1).doubleValue() -
+                (gamma + mu) * systemVars.get(1).doubleValue(); });
+
+        functions.add( (systemVars) -> {return gamma*systemVars.get(1).doubleValue() - mu*systemVars.get(2).doubleValue(); });
+
+        return new ODESystem(initialStateVector, functions);
+
+    }
+
+    private ODESystem SIR(double initialS, double initialI, double initialR) {
+
+        double k = 3;
+        double mu = 0.001;
+        double gamma = 2;
+
         ArrayList<Number> initialStateVector = new ArrayList<>();
         ArrayList<IFunc<Number, Number>> functions = new ArrayList<>();
 
