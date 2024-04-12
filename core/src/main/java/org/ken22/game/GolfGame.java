b98518799d0c9fdd2de35129f3f702c369b1b3fe
Andrei <87477311+cartesian-plane@users.noreply.github.com>
2024-04-12 22:30:07 +0200

    libgdx liftoff

---
+++ core/src/main/java/org/ken22/game/GolfGame.java
@@ -0,0 +1,11 @@
+package org.ken22.game;
+
+import com.badlogic.gdx.Game;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class GolfGame extends Game {
+    @Override
+    public void create() {
+        setScreen(new FirstScreen());
+    }
+}
