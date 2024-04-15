c7571cf157c5ca01b7eb07692d348e8dc93de72e
Andrei <87477311+cartesian-plane@users.noreply.github.com>
2024-04-15 18:40:19 +0200

    Add CourseParser example code

--- core/src/main/java/org/ken22/input/courseinput/CourseParser.java
+++ core/src/main/java/org/ken22/input/courseinput/CourseParser.java
@@ -1,4 +1,29 @@
 package org.ken22.input.courseinput;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
 public class CourseParser {
+    public static void main(String[] args) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        File jsonFile = new File("assets/input/golf-course.json");
+        try {
+
+            JsonNode jsonNode = objectMapper.readTree(jsonFile);
+            JsonNode golfCourseNode = jsonNode.get("golfCourse");
+            String terrain = golfCourseNode.get("courseProfile").asText();
+            System.out.println("Terrain: " + terrain);
+        } catch (
+            IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void doSomething() {
+
+    }
+
 }
