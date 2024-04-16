c4df4013418225ae21cac62038813423aa419220
Andrei <87477311+cartesian-plane@users.noreply.github.com>
2024-04-16 13:04:53 +0200

    Add example course parsing code

--- core/src/main/java/org/ken22/input/courseinput/CourseParser.java
+++ core/src/main/java/org/ken22/input/courseinput/CourseParser.java
@@ -2,9 +2,13 @@ package org.ken22.input.courseinput;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CourseParser {
     public static void main(String[] args) {
@@ -16,14 +20,20 @@ public class CourseParser {
             JsonNode golfCourseNode = jsonNode.get("golfCourse");
             String terrain = golfCourseNode.get("courseProfile").asText();
             System.out.println("Terrain: " + terrain);
+
+            Expression expression = new ExpressionBuilder(terrain)
+                .variables("x", "y")
+                .build()
+                .setVariable("x", 1)
+                .setVariable("y", 2);
+            double result = expression.evaluate();
+            System.out.println("Terrain at (" + 1 + ", " + 2 + ")" + "= " + result);
+            double trueResult = Math.sin( (1.0 - 2) / 7) + 0.5;
+            System.out.println("True result: " + trueResult);
         } catch (
             IOException e) {
             e.printStackTrace();
         }
-    }
-
-    private void doSomething() {
-
     }
 
 }
