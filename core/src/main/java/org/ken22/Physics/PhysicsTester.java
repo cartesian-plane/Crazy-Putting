7f8486f2a9f4cfed1af1f3becc387d3d57b08161
ø__o <52105250+leo07w@users.noreply.github.com>
2024-04-23 10:18:35 +0200

    Added physics engine and tester

---
+++ core/src/main/java/org/ken22/Physics/PhysicsTester.java
@@ -0,0 +1,69 @@
+package org.ken22.Physics;
+
+import org.ken22.interfaces.IFunc;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class PhysicsTester { //megaShit is the output of megaAss, it is a tester class for megaAss
+    public static void main(String[] args) {
+
+        //double x, double y, double vx, double vy, double timeStep, double startTime, double endTime, double kFrictionCoef, double sFrictionCoef, double gCoef, IFunc<Double, Double> height
+        IFunc<Double, Double> testFunc1 = (vars) -> (2*Math.sin(vars.get(0))*Math.sin(vars.get(1)));
+        PhysicsEngine test1 = new PhysicsEngine(1,1,1,1, 0.001, 0, 1, 0.1, 0.2, 9.81, testFunc1);
+        test1.solve();
+
+        IFunc<Double, Double> testFunc2 = (vars) -> ((vars.get(0)+vars.get(1))/2);
+        PhysicsEngine test2 = new PhysicsEngine(4,4,1,1, 0.001, 0, 3, 0.1, 0.2, 9.81, testFunc2);
+        test2.solve();
+        System.out.println(test2.getStateVectors());
+
+    }
+
+    public static void testSystem(PhysicsEngine engine) throws IOException {
+
+        engine.solve();
+        ArrayList<ArrayList<Double>> vectors = new ArrayList<ArrayList<Double>>();
+        vectors = engine.getStateVectors();
+
+        try {
+            FileWriter csvWriter = new FileWriter("/Users/leo/Desktop/Output/errorseuler.csv");
+
+            // Write header
+            csvWriter.append("time");
+            csvWriter.append(",");
+            csvWriter.append("x");
+            csvWriter.append(",");
+            csvWriter.append("y");
+            csvWriter.append(",");
+            csvWriter.append("vx");
+            csvWriter.append(",");
+            csvWriter.append("vy");
+            csvWriter.append(",");
+            csvWriter.append("dhdx");
+            csvWriter.append(",");
+            csvWriter.append("dhdy");
+            csvWriter.append("\n");
+
+            // Write data
+            for (ArrayList<Double> vector : vectors) {
+                for (Double value : vector) {
+                    csvWriter.append(String.valueOf(value));
+                    csvWriter.append(",");
+                }
+                csvWriter.append("\n");
+            }
+
+            csvWriter.flush();
+            csvWriter.close();
+
+            System.out.println("CSV file written successfully.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+
