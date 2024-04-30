ca2ef41ab91b558baf2c56a501564c587287ce3e
Andrei <87477311+cartesian-plane@users.noreply.github.com>
2024-04-30 10:29:59 +0200

    Change HdpiMode to Pixels

--- lwjgl3/src/main/java/org/ken22/game/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/org/ken22/game/lwjgl3/Lwjgl3Launcher.java
@@ -2,6 +2,7 @@ package org.ken22.game.lwjgl3;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.graphics.glutils.HdpiMode;
 import org.ken22.game.GolfGame;
 
 /** Launches the desktop (LWJGL3) application. */
@@ -25,6 +26,7 @@ public class Lwjgl3Launcher {
         //// useful for testing performance, but can also be very stressful to some hardware.
         //// You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
         configuration.setWindowedMode(640, 480);
+        configuration.setHdpiMode(HdpiMode.Pixels);
         configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
         return configuration;
     }
