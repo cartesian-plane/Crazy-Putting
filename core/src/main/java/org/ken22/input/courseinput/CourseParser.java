74fbd9eed5d96a785ec558de522bba3530a9c6f3
Andrei <87477311+cartesian-plane@users.noreply.github.com>
2024-05-01 17:20:44 +0200

    Sync input module files

--- core/src/main/java/org/ken22/input/courseinput/CourseParser.java
+++ core/src/main/java/org/ken22/input/courseinput/CourseParser.java
@@ -1,4 +1,41 @@
 package org.ken22.input.courseinput;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class CourseParser {
+    public static void main(String[] args) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        File jsonFile = new File("assets/input/golf-course.json");
+        try {
+
+            String terrain = "sin((x - y) / 7) + 0.5";
+            System.out.println("Terrain: " + terrain);
+
+            GolfCourse course = objectMapper.readValue(jsonFile, GolfCourse.class);
+
+            System.out.println("Mass: " + course.getMass());
+
+            Expression expression = new ExpressionBuilder(terrain)
+                .variables("x", "y")
+                .build()
+                .setVariable("x", 1)
+                .setVariable("y", 2);
+            double result = expression.evaluate();
+            System.out.println("Terrain at (" + 1 + ", " + 2 + ")" + "= " + result);
+            double trueResult = Math.sin( (1.0 - 2) / 7) + 0.5;
+            System.out.println("True result: " + trueResult);
+        } catch (
+            IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
---
+++ core/src/main/java/org/ken22/input/courseinput/InputGuide.md
@@ -0,0 +1,13 @@
+# Input Guide
+
+>All of the required information can be found in the project manual; this is just a short summary of that.
+
+
+## Units:
+* mass - in kg
+* gravitationalConstant - in m/s
+* maximumSpeed - m/s
+* targetRadius - m
+
+The rest should be obvious. It would be nice to add input validation, to make sure the user input is within
+the manual's restrictions; however, it's not necessary.
