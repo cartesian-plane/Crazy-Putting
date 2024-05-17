f331a012125ae05b7e18e393eb2398a53b51ddee
Andrei <87477311+cartesian-plane@users.noreply.github.com>
2024-05-17 12:48:36 +0200

    Revise Physics
    The latest commit did not bring everything :(
    There was just a small thing with the golf course parser, but this commit fixes that

---
+++ core/src/main/java/org/ken22/Physics/Vectors/Vec.java
@@ -0,0 +1,10 @@
+package org.ken22.Physics.Vectors;
+
+import java.util.ArrayList;
+
+public interface Vec {
+
+    int size();
+
+    ArrayList<Double> getVector();
+}
--- core/src/main/java/org/ken22/input/courseinput/CourseParser.java
+++ core/src/main/java/org/ken22/input/courseinput/CourseParser.java
@@ -11,31 +11,68 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CourseParser {
-    public static void main(String[] args) {
+
+    private GolfCourse course;
+    private Map<String, Double> variables = new HashMap<>();
+    private Expression expression;
+    private String terrain;
+
+    public CourseParser(File jsonFile) {
+
         ObjectMapper objectMapper = new ObjectMapper();
-        File jsonFile = new File("assets/input/golf-course.json");
         try {
-
-            String terrain = "sin((x - y) / 7) + 0.5";
-            System.out.println("Terrain: " + terrain);
-
-            GolfCourse course = objectMapper.readValue(jsonFile, GolfCourse.class);
-
-            System.out.println("Mass: " + course.mass());
-
-            Expression expression = new ExpressionBuilder(terrain)
+            this.course = objectMapper.readValue(jsonFile, GolfCourse.class);
+            this.terrain = course.courseProfile();
+            this.expression = new ExpressionBuilder(terrain)
                 .variables("x", "y")
-                .build()
-                .setVariable("x", 1)
-                .setVariable("y", 2);
-            double result = expression.evaluate();
-            System.out.println("Terrain at (" + 1 + ", " + 2 + ")" + "= " + result);
-            double trueResult = Math.sin( (1.0 - 2) / 7) + 0.5;
-            System.out.println("True result: " + trueResult);
+                .build();
         } catch (
             IOException e) {
             e.printStackTrace();
         }
     }
+
+    public GolfCourse getCourse() {
+        return course;
+    }
+
+    public Map<String, Double> getVariables() {
+        return variables;
+    }
+
+    public Expression getExpression() {
+        return expression;
+    }
+
+    public String getTerrain() {
+        return terrain;
+    }
+
+    //    public static void main(String[] args) {
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        File jsonFile = new File("project-1-2/assets/input/golf-course.json");
+//        try {
+//
+//            String terrain = "sin((x - y) / 7) + 0.5";
+//            System.out.println("Terrain: " + terrain);
+//
+//            GolfCourse course = objectMapper.readValue(jsonFile, GolfCourse.class);
+//
+//            System.out.println("Mass: " + course.getMass());
+//
+//            Expression expression = new ExpressionBuilder(terrain)
+//                .variables("x", "y")
+//                .build()
+//                .setVariable("x", 1)
+//                .setVariable("y", 2);
+//            double result = expression.evaluate();
+//            System.out.println("Terrain at (" + 1 + ", " + 2 + ")" + "= " + result);
+//            double trueResult = Math.sin( (1.0 - 2) / 7) + 0.5;
+//            System.out.println("True result: " + trueResult);
+//        } catch (
+//            IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
 }
