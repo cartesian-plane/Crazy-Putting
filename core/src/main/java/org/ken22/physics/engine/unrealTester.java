dc26d703d92f2ad4a4f0debf81faa8302ec0240d
ø__o <52105250+leo07w@users.noreply.github.com>
2024-05-18 15:08:55 +0200

    Fixed some naming issues

--- core/src/main/java/org/ken22/physics/engine/unrealTester.java
+++ core/src/main/java/org/ken22/physics/engine/unrealTester.java
@@ -1,12 +1,11 @@
-package org.ken22.Physics.Engine;
+package org.ken22.physics.engine;
 
-import org.ken22.Physics.Numerical_Derivation.NumDerivationMethod;
-import org.ken22.Physics.Numerical_Derivation.basicDerivation;
-import org.ken22.Physics.Numerical_Derivation.fivePointDifference;
-import org.ken22.Physics.Numerical_Integration.NumIntegrationMethod;
-import org.ken22.Physics.Numerical_Integration.RK4;
-import org.ken22.Physics.System.PhysicsSystem;
-import org.ken22.Physics.Vectors.GVec4;
+import org.ken22.physics.numerical_derivation.NumDerivationMethod;
+import org.ken22.physics.numerical_derivation.fivePointDifference;
+import org.ken22.physics.numerical_integration.NumIntegrationMethod;
+import org.ken22.physics.numerical_integration.RK4;
+import org.ken22.physics.system.PhysicsSystem;
+import org.ken22.physics.vectors.GVec4;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
