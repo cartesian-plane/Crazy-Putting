66a7ed80fab6e8839b495625a75fb2eb0e83c526
Andrei <87477311+cartesian-plane@users.noreply.github.com>
2024-05-19 18:47:32 +0200

    Add example methods

---
+++ core/src/main/java/org/ken22/physicsx/PhysicsEngine.java
@@ -0,0 +1,53 @@
+package org.ken22.physicsx;
+
+import org.ken22.input.courseinput.GolfCourse;
+import org.ken22.physicsx.odesolvers.ODESolver;
+import org.ken22.physicsx.vectors.StateVector4;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class PhysicsEngine {
+
+    private GolfCourse course;
+    private StateVector4 initialStateVector;
+    private double timeStep;
+    private Differentiator differentiator;
+    private ODESolver solver;
+    public Iterator frameIterator implements Iterator;
+    private ArrayList<StateVector4> trajectory;
+
+
+
+    public PhysicsEngine(GolfCourse course, StateVector4 initialStateVector) {
+        this.course = course;
+        this.initialStateVector = initialStateVector;
+        this.timeStep = 0.0001;
+        trajectory.add(initialStateVector);
+    }
+
+    public PhysicsEngine(GolfCourse course, StateVector4 initialStateVector, double timeStep,
+                         Differentiator differentiator, ODESolver solver) {
+
+        if (timeStep < 0.016) {
+            throw new IllegalArgumentException("Step size too big for 60FPS");
+        }
+
+        this.course = course;
+        this.initialStateVector = initialStateVector;
+        this.timeStep = timeStep;
+        this.differentiator = differentiator(GolfCourse);
+        this.solver = solver(GolfCourse);
+        trajectory.add(initialStateVector);
+    }
+
+    public boolean isAtRest() {
+        return true;
+    }
+
+    private StateVector4 nextStep() {
+        StateVector4 lastVector = trajectory.getLast();
+
+        solver.nextStep(timeStep, lastVector, Vecto);
+    }
+}
