fbe1562c2fb718fe1d084cc838d34fa7aeb78423
Andrei <87477311+cartesian-plane@users.noreply.github.com>
2024-05-19 19:48:24 +0200

    Load golf-course.json into unit test

--- core/src/test/java/org/ken22/physicsx/differentiation/VectorDifferentiationFactoryTest.java
+++ core/src/test/java/org/ken22/physicsx/differentiation/VectorDifferentiationFactoryTest.java
@@ -1,15 +1,36 @@
 package org.ken22.physicsx.differentiation;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.ken22.input.courseinput.GolfCourse;
+
+import java.io.File;
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 class VectorDifferentiationFactoryTest {
 
 
     @Test
     @DisplayName("Dummy test")
     void vectorDifferentiation4() {
+        GolfCourse course;
+
+        File resourcesDirectory = new File("src/test/resources");
+        File courseFile = new File(resourcesDirectory, "golf-course.json");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            course = objectMapper.readValue(courseFile, GolfCourse.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         assertEquals(true, true);
     }
+
 }
+
