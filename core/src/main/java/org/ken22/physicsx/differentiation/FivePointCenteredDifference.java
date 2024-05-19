9bc47e4c53c9df9bb2075f16aa8842248c4bb77d
Mihai Solescu <mihai.solescu@outlook.com>
2024-05-19 19:28:25 +0200

    VectorDifferentiatorFactory; PhysicsEngine iterator and nextStep()

--- core/src/main/java/org/ken22/physicsx/differentiation/FivePointCenteredDifference.java
+++ core/src/main/java/org/ken22/physicsx/differentiation/FivePointCenteredDifference.java
@@ -2,8 +2,8 @@ package org.ken22.physicsx.differentiation;
 
 import java.util.function.Function;
 
-public class FivePointCenteredDifference {
-    public static double differentiate(double h, double x, Function<Double, Double> f) {
+public class FivePointCenteredDifference implements Differentiator {
+    public double differentiate(double h, double x, Function<Double, Double> f) {
         return (f.apply(x - 2 * h) - 8 * f.apply(x - h) + 8 * f.apply(x + h) - f.apply(x + 2 * h)) / (12 * h);
     }
 }
---
+++ core/src/main/java/org/ken22/physicsx/engine/PhysicsEngine.java
@@ -0,0 +1,104 @@
+package org.ken22.physicsx.engine;
+
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+import org.ken22.input.courseinput.GolfCourse;
+import org.ken22.physicsx.differentiation.Differentiator;
+import org.ken22.physicsx.differentiation.FivePointCenteredDifference;
+import org.ken22.physicsx.differentiation.VectorDifferentiation4;
+import org.ken22.physicsx.differentiation.VectorDifferentiationFactory;
+import org.ken22.physicsx.odesolvers.ODESolver;
+import org.ken22.physicsx.odesolvers.RK4;
+import org.ken22.physicsx.vectors.StateVector4;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class PhysicsEngine {
+
+    private static final double DEFAULT_TIME_STEP = 0.00001;
+    private final GolfCourse course;
+    private final Expression expr;
+
+    private final VectorDifferentiationFactory vectorDifferentiationFactory;
+    private final ODESolver solver;
+    private final Differentiator differentiator;
+
+    private ArrayList<StateVector4> trajectory = new ArrayList<>();
+    private StateVector4 initialStateVector;
+    private double timeStep; // Default time step
+
+
+
+    /// Constructors
+    public PhysicsEngine(GolfCourse course, StateVector4 initialStateVector) {
+        this(course, initialStateVector, DEFAULT_TIME_STEP, new FivePointCenteredDifference(), new RK4());
+    }
+
+    public PhysicsEngine(GolfCourse course, StateVector4 initialStateVector, double timeStep,
+                         Differentiator differentiator, ODESolver solver) {
+
+        if (timeStep < 0.016) {
+            throw new IllegalArgumentException("Step size too big for 60FPS");
+        }
+
+        this.course = course;
+        this.initialStateVector = initialStateVector;
+        this.timeStep = timeStep;
+        this.differentiator = differentiator;
+        this.solver = solver;
+        this.expr = new ExpressionBuilder(course.courseProfile())
+            .variables("x", "y")
+            .build();
+        this.vectorDifferentiationFactory = new VectorDifferentiationFactory(timeStep, expr, course, differentiator);
+        trajectory.add(initialStateVector);
+    }
+
+
+    /// Methods
+    public boolean isAtRest() {
+        StateVector4 lastVector = trajectory.getLast();
+        return false;
+    }
+
+    /**
+     * Generates, appends and returns the next state vector in the trajectory according to the step size
+     * Does not check whether the ball is at rest
+     * @return
+     */
+    private StateVector4 nextStep() {
+        StateVector4 lastVector = trajectory.getLast();
+        VectorDifferentiation4 differentiation = vectorDifferentiationFactory.vectorDifferentiation4(lastVector.x(), lastVector.y());
+        StateVector4 newVector = solver.nextStep(timeStep, lastVector, differentiation);
+        trajectory.add(newVector);
+        return newVector;
+    }
+
+    /**
+     * Returns an iterator that will iterate over the trajectory of the golf ball for graphical display
+     *
+     * Assumes that time matches dt = 1 -> 1s has passed, and frame rate of 60FPS
+     */
+    public class frameRateIterator implements Iterator<StateVector4> {
+        private static int FRAME_RATE = 60;
+        private double kPerFrame = (1.0/FRAME_RATE) / timeStep;
+        private int index = 0;
+
+
+        @Override
+        public boolean hasNext() {
+            return isAtRest();
+        }
+
+        @Override
+        public StateVector4 next() {
+            for(int i = 0; i < kPerFrame; i++) {
+                nextStep();
+                if(!isAtRest()) {
+                    break;
+                }
+            }
+            return trajectory.getLast();
+        }
+    }
+}
