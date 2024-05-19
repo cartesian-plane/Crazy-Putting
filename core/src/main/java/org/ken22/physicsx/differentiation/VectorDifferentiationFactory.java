1785bd1dc89a4fda8f961141af4df223798d97f2
Mihai Solescu <mihai.solescu@outlook.com>
2024-05-19 19:51:45 +0200

    VectorDifferentiatorFactory - added low speed differentiation

--- core/src/main/java/org/ken22/physicsx/differentiation/VectorDifferentiationFactory.java
+++ core/src/main/java/org/ken22/physicsx/differentiation/VectorDifferentiationFactory.java
@@ -34,11 +34,32 @@ public class VectorDifferentiationFactory {
 
         // Define the velocity differentiation functions
         Function<StateVector4, Double> dvx = (stateVector4) ->
-            -course.gravitationalConstant() * df_dx - course.kineticFrictionGrass() * stateVector4.vx() /
-                magnitude(stateVector4.vx(), stateVector4.vy());
+            (-course.gravitationalConstant() * df_dx - course.kineticFrictionGrass() * stateVector4.vx() /
+                magnitude(stateVector4.vx(), stateVector4.vy()))/course.mass();
         Function<StateVector4, Double> dvy = (stateVector4) ->
-            -course.gravitationalConstant() * df_dy - course.kineticFrictionGrass() * stateVector4.vy() /
-                magnitude(stateVector4.vx(), stateVector4.vy());
+            (-course.gravitationalConstant() * df_dy - course.kineticFrictionGrass() * stateVector4.vy() /
+                magnitude(stateVector4.vx(), stateVector4.vy()))/course.mass();
+
+        // Return the vector differentiation object
+        return new VectorDifferentiation4(dx, dy, dvx, dvy);
+    }
+
+    public VectorDifferentiation4 lowSpeedVectorDifferentiation4(double xCoord, double yCoord) {
+        // Define the univariate functions at the x and y coordinates
+        Function<Double, Double> fx = (x) -> expr.setVariable("y", yCoord).setVariable("x", x).evaluate();
+        Function<Double, Double> fy = (y) -> expr.setVariable("x", xCoord).setVariable("y", y).evaluate();
+
+        // Aproximate the derivatives of the functions at the x and y coordinates
+        double df_dx = differentiator.differentiate(h, xCoord, fx);
+        double df_dy = differentiator.differentiate(h, yCoord, fy);
+
+        // Define the velocity differentiation functions
+        Function<StateVector4, Double> dvx = (stateVector4) ->
+            (-course.gravitationalConstant() * df_dx - course.kineticFrictionGrass() * df_dx /
+            magnitude(df_dx, df_dy))/course.mass();
+        Function<StateVector4, Double> dvy = (stateVector4) ->
+            (-course.gravitationalConstant() * df_dy - course.kineticFrictionGrass() * df_dy /
+                magnitude(df_dx, df_dy))/course.mass();
 
         // Return the vector differentiation object
         return new VectorDifferentiation4(dx, dy, dvx, dvy);
