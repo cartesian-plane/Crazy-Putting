54e4d734545c155c23089712e16ab2c240fd71a3
Mihai Solescu <mihai.solescu@outlook.com>
2024-05-21 09:14:40 +0200

    testing

--- core/src/test/java/org/ken22/physicsx/engine/PhysicsEngineTest.java
+++ core/src/test/java/org/ken22/physicsx/engine/PhysicsEngineTest.java
@@ -1,6 +1,7 @@
 package org.ken22.physicsx.engine;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.ken22.input.courseinput.GolfCourse;
 import org.ken22.physicsx.vectors.StateVector4;
@@ -14,6 +15,32 @@ import static org.junit.jupiter.api.Assertions.*;
 class PhysicsEngineTest {
 
     @Test
+    @DisplayName("Underwater collision")
+    void underwater() {
+        GolfCourse course;
+
+        File resourcesDirectory = new File("src/test/resources");
+        File courseFile = new File(resourcesDirectory, "golf-course.json");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            course = objectMapper.readValue(courseFile, GolfCourse.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Start the ball at a negative height
+        StateVector4 initialState = new StateVector4(-2, 0, 0, 0);
+        PhysicsEngine engine = new PhysicsEngine(course, initialState);
+
+        assertTrue(engine.underwater());
+    }
+
+    @Test
+    /**
+     * Not actually testing isAtRest(). Just running the iterator.
+     */
     void isAtRest() {
         GolfCourse course = course();
         // Expression expr = expr(course);
