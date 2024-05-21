7ac12e51e64822334a550f7e8f2aa35d2bad6746
Mihai Solescu <mihai.solescu@outlook.com>
2024-05-21 09:12:35 +0200

    testing

--- core/src/test/java/org/ken22/physicsx/engine/PhysicsEngineTest.java
+++ core/src/test/java/org/ken22/physicsx/engine/PhysicsEngineTest.java
@@ -1,25 +1,36 @@
 package org.ken22.physicsx.engine;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.ken22.input.courseinput.GolfCourse;
 import org.ken22.physicsx.vectors.StateVector4;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Iterator;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PhysicsEngineTest {
 
     @Test
-    @DisplayName("Underwater collision")
-    void underwater() {
+    void isAtRest() {
+        GolfCourse course = course();
+        // Expression expr = expr(course);
+        StateVector4 initialStateVector = new StateVector4(0.0, 0.0, 1.0, 1.0);
+        PhysicsEngine engine = new PhysicsEngine(course, initialStateVector);
+        Iterator<StateVector4> iterator = engine.new frameRateIterator();
+        while (iterator.hasNext()) {
+            StateVector4 stateVector4 = iterator.next();
+            System.out.println(stateVector4.toString());
+        }
+    }
+
+    GolfCourse course() {
         GolfCourse course;
 
         File resourcesDirectory = new File("src/test/resources");
-        File courseFile = new File(resourcesDirectory, "golf-course.json");
+        File courseFile = new File(resourcesDirectory, "true-golf-course.json");
 
         ObjectMapper objectMapper = new ObjectMapper();
 
@@ -28,11 +39,6 @@ class PhysicsEngineTest {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-        // Start the ball at a negative height
-        StateVector4 initialState = new StateVector4(-2, 0, 0, 0);
-        PhysicsEngine engine = new PhysicsEngine(course, initialState);
-
-        assertTrue(engine.underwater());
+        return course;
     }
 }
