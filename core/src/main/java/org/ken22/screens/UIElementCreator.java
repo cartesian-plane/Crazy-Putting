c520799b25242f318f404e1b277a3eb4ab60dde5
Andrei <a.chiochiu@student.maastrichtuniversity.nl>
2024-06-13 19:01:42 +0200

    Add numerical text field generation

--- core/src/main/java/org/ken22/screens/UIElementCreator.java
+++ core/src/main/java/org/ken22/screens/UIElementCreator.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 /**
@@ -34,6 +35,13 @@ public class UIElementCreator {
             }
         });
         return button;
+    }
+
+    public static TextField createNumericalTextField(String text, Skin skin) {
+        var numericalField = new TextField(text, skin);
+        numericalField.setTextFieldFilter(new TextField.TextFieldFilter.DigitsOnlyFilter());
+
+        return numericalField;
     }
 
 }
