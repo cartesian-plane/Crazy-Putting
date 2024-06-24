90a635d3481eb174fb2752bdf15526e6456c2b89
Mihai Solescu <mihai.solescu@outlook.com>
2024-06-24 13:43:52 +0200

    In place odesolver

--- core/src/main/java/org/ken22/physics/differentiation/inplace/VectorDifferentiationFactory.java
+++ core/src/main/java/org/ken22/physics/differentiation/inplace/VectorDifferentiationFactory.java
@@ -44,27 +44,53 @@ public class VectorDifferentiationFactory {
                 double df_dy = ySlope(vx, vy);
                 double d_norm = MathUtils.magnitude(1, df_dx, df_dy);
                 double big_term = MathUtils.magnitude(vx, vy, df_dx * vx + df_dy * vy);
-                double accx = -(this.g/d_norm) * (df_dx/d_norm + this.kf_g * vx / (d_norm*big_term));
-                double accy = -(this.g/d_norm) * (df_dy/d_norm + this.kf_g * vy / (d_norm*big_term));
+                double accx = -(this.g / d_norm) * (df_dx / d_norm + this.kf_g * vx / (d_norm * big_term));
+                double accy = -(this.g / d_norm) * (df_dy / d_norm + this.kf_g * vy / (d_norm * big_term));
+
+                // Approximation:
+                der[0] = sv.x() + h * sv.vx();
+                der[1] = sv.y() + h * sv.vy();
+                der[2] = sv.vx() + h * accx;
+                der[3] = sv.vy() + h * accy;
+
                 // Derivative:
-
-
+                df_dx = xSlope(der[0], der[1]);
+                df_dy = ySlope(der[0], der[1]);
+                d_norm = MathUtils.magnitude(1, df_dx, df_dy);
+                big_term = MathUtils.magnitude(df_dx * der[2] + df_dy * der[3]);
+                der[0] = der[2];
+                der[1] = der[3];
+                der[2] = -(this.g / d_norm) * (df_dx / d_norm + this.kf_g * der[2] / (d_norm * big_term));
+                der[3] = -(this.g / d_norm) * (df_dy / d_norm + this.kf_g * der[3] / (d_norm * big_term));
 
                 return der;
             };
         else
             runStep = (h, sv) -> {
                 double[] der = new double[4];
+
                 double df_dx = xSlope(sv.x(), sv.y());
                 double df_dy = ySlope(sv.x(), sv.y());
                 double v_norm = MathUtils.magnitude(sv.vx(), sv.vy());
+                //acceleration
+                double accx = -course.gravitationalConstant() * (df_dx + course.kineticFrictionGrass() * sv.vx() / v_norm);
+                double accy = -course.gravitationalConstant() * (df_dy + course.kineticFrictionGrass() * sv.vy() / v_norm);
+
+                // sv approximation at t + h // temporarily stored in der for efficiency
+                der[0] = sv.x() + h * sv.vx();
+                der[1] = sv.y() + h * sv.vy();
+                der[2] = sv.vx() + h * accx;
+                der[3] = sv.vy() + h * accy;
+
                 // Derivative:
-//                return new StateVector4(
-//                    sv.vx(),
-//                    sv.vy(),
-//                    (-course.gravitationalConstant() * (df_dx + course.kineticFrictionGrass() * sv.vx() / v_norm)),
-//                    (-course.gravitationalConstant() * (df_dy + course.kineticFrictionGrass() * sv.vy() / v_norm))
-//                );
+                der[0] = der[2];
+                der[1] = der[3];
+                df_dx = xSlope(der[0], der[0]);
+                df_dy = ySlope(der[0], der[0]);
+                v_norm = MathUtils.magnitude(der[2], der[3]);
+                der[3] = -course.gravitationalConstant() * (df_dx + course.kineticFrictionGrass() * der[2] / v_norm);
+                der[4] = -course.gravitationalConstant() * (df_dy + course.kineticFrictionGrass() * der[3] / v_norm);
+
                 return der;
             };
         return runStep;
@@ -75,6 +101,7 @@ public class VectorDifferentiationFactory {
             if (complete)
                 runStep = (h, sv) -> {
                     double[] der = new double[4];
+
                     double vx = sv.vx();
                     double vy = sv.vy();
                     double df_dx = xSlope(vx, vy);
@@ -82,28 +109,54 @@ public class VectorDifferentiationFactory {
                     double d_norm = MathUtils.magnitude(1, df_dx, df_dy);
                     // approximation not from booklet vx, vy -> dh_dx, dh_dy and vx^2, vy^2 -> 0
                     double big_term = MathUtils.magnitude(df_dy * df_dx + df_dy * df_dy);
+                    //acceleration
+                    double accx = -(this.g * df_dx / d_norm) * (1/d_norm + this.kf_g / (d_norm * big_term));
+                    double accy = -(this.g * df_dy / d_norm) * (1/d_norm + this.kf_g / (d_norm * big_term));
+
+                    // Approximation:
+                    der[0] = sv.x() + h * sv.vx();
+                    der[1] = sv.y() + h * sv.vy();
+                    der[2] = sv.vx() + h * accx;
+                    der[3] = sv.vy() + h * accy;
+
                     // Derivative:
-//                return new StateVector4(
-//                    vx,
-//                    vy,
-//                    (-(this.g*df_dx/d_norm) * (1/d_norm + this.kf_g / (d_norm*big_term))),
-//                    (-(this.g*df_dy/d_norm) * (1/d_norm + this.kf_g / (d_norm*big_term)))
-//                );
+                    df_dx = xSlope(der[0], der[1]);
+                    df_dy = ySlope(der[0], der[1]);
+                    d_norm = MathUtils.magnitude(1, df_dx, df_dy);
+                    big_term = MathUtils.magnitude(df_dy * df_dx + df_dy * df_dy);
+                    der[0] = der[2];
+                    der[1] = der[3];
+                    der[2] = -(this.g * df_dx / d_norm) * (1/d_norm + this.kf_g / (d_norm * big_term));
+                    der[3] = -(this.g * df_dy / d_norm) * (1/d_norm + this.kf_g / (d_norm * big_term));
+
                     return der;
                 };
         else
             runStep = (h, sv) -> {
                 double[] der = new double[4];
+
                 double df_dx = xSlope(sv.x(), sv.y());
                 double df_dy = ySlope(sv.x(), sv.y());
                 double d_norm = MathUtils.magnitude(df_dx, df_dy);
+                //acceleration
+                double accx = -course.gravitationalConstant() * (df_dx + course.kineticFrictionGrass() * df_dx / d_norm);
+                double accy = -course.gravitationalConstant() * (df_dy + course.kineticFrictionGrass() * df_dy / d_norm);
+
+                // approximation
+                der[0] = sv.x() + h * sv.vx();
+                der[1] = sv.y() + h * sv.vy();
+                der[2] = sv.vx() + h * accx;
+                der[3] = sv.vy() + h * accy;
+
                 // Derivative:
-//                return new StateVector4(
-//                    sv.vx(),
-//                    sv.vy(),
-//                    (-course.gravitationalConstant() * (df_dx + course.kineticFrictionGrass() * df_dx / d_norm)),
-//                    (-course.gravitationalConstant() * (df_dy + course.kineticFrictionGrass() * df_dy / d_norm))
-//                );
+                df_dx = xSlope(der[0], der[1]);
+                df_dy = ySlope(der[0], der[1]);
+                d_norm = MathUtils.magnitude(df_dx, df_dy);
+                der[0] = der[2];
+                der[1] = der[3];
+                der[2] = -course.gravitationalConstant() * (df_dx + course.kineticFrictionGrass() * df_dx / d_norm);
+                der[3] = -course.gravitationalConstant() * (df_dy + course.kineticFrictionGrass() * df_dy / d_norm);
+
                 return der;
             };
         return runStep;
--- core/src/main/java/org/ken22/physics/odesolvers/inplace/ODESolver.java
+++ core/src/main/java/org/ken22/physics/odesolvers/inplace/ODESolver.java
@@ -4,6 +4,7 @@ import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 
 public interface ODESolver<State> {
-    public void solve(double timeStep, double endTime, State initialState, BiFunction<Double, State, double[]> differentiation); //
+    // the differentiation returns a double[4] of the derivative of the statevector at t + h
+    public void solve(double timeStep, double endTime, State initialState, BiFunction<Double, State, double[]> differentiation);
     public void nextStep(double timeStep, State currentState, BiFunction<Double, State, double[]> differentiation);
 }
