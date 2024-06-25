c6b3b7e15e3b7fac96db9f8744b7deaf1e0f1a33
Andrei <a.chiochiu@student.maastrichtuniversity.nl>
2024-06-25 20:45:06 +0200

    Add InjectedExpression interface

---
+++ core/src/main/java/org/ken22/input/InjectedExpression.java
@@ -0,0 +1,8 @@
+package org.ken22.input;
+
+/**
+ * Interface implemented by the compiled clas injected at runtime.
+ */
+public interface InjectedExpression {
+    double evaluate(double x, double y);
+}
